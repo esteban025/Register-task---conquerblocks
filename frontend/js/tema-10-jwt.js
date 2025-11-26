ca7d683c6a60441072211d57220fc20d01052561
EstebanDev <guachichullcaesteban@gmail.com>
2025-11-26 11:44:57 -0500

    JWT

---
+++ frontend/js/tema-10-jwt.js
@@ -0,0 +1,14 @@
+// === JWT ===
+
+// que es?
+// JWT significa JSON Web Token
+
+// es un estándar abierto (RFC 7519) que define una forma compacta y autónoma de transmitir información segura entre partes como un objeto JSON
+
+// esta información puede ser verificada y confiable porque está firmada digitalmente
+
+// estructura
+// un JWT típicamente consta de tres partes separadas por puntos (.), que son:
+// 1. Header (Encabezado)
+// 2. Payload (Carga útil)
+// 3. Signature (Firma)
